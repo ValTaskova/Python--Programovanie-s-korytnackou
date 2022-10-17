d6096059571dadc025f605b4f251b29e4a494dc2
ValTaskova <93611731+ValTaskova@users.noreply.github.com>
2022-10-17 17:37:53 +0200

    Create cv3.py

---
+++ 3_hodina/cv3.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+showturtle()
+
+write(pos())    #write('ciara')
+
+forward(100)    #posunie o 100 dopredu
+forward(100)    #posunie o dalsich 100 dopredu
+goto(300, 100)  #idem na suradnice X = 300 a Y = 100
+goto(0, 0)      #idem na suradnice X = 0 a Y = 0
+
+write(pos())    # vypise mi suradnice
+
+done()
