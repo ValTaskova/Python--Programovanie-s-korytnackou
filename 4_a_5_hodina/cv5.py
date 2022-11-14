a3c67f1b59ecc0f504c851ad7198ad858fb5c956
ValTaskova <93611731+ValTaskova@users.noreply.github.com>
2022-11-14 15:36:33 +0100

    Create cv5.py

---
+++ 4_a_5_hodina/cv5.py
@@ -0,0 +1,45 @@
+from turtle import *
+
+showturtle()
+
+dlzka = 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+dlzka = dlzka + 5
+forward(dlzka)
+right(90)
+
+done()
