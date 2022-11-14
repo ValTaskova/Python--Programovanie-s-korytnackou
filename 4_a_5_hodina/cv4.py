287028e6baebfe877b561607a584573021c78a26
ValTaskova <93611731+ValTaskova@users.noreply.github.com>
2022-11-14 15:35:57 +0100

    Create cv4.py

---
+++ 4_a_5_hodina/cv4.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+strana1 = 300           #premenna strana1 ma hodnotu 300
+strana2 = strana1/2     #premenna strana2 ma hodnotu polovicu zo strana1, teda 150
+uholStvorca = 90        #premenna uholStvorca ma hodnotu 90
+
+strana1 = 200           #premenna strana1 ma teraz nastavenu hodnotu 200
+
+forward(strana1)
+left(uholStvorca)
+
+forward(strana2)
+left(uholStvorca)
+
+
+forward(strana1)
+left(uholStvorca)
+
+forward(strana2)
+left(uholStvorca)
+
+done()
