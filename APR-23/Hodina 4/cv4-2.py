35e8f5f533be68532b3d9c92e61949f81c5b84aa
ValTaskova <93611731+ValTaskova@users.noreply.github.com>
2023-06-01 14:18:23 +0200

    Create cv4-2.py

---
+++ APR-23/Hodina 4/cv4-2.py
@@ -0,0 +1,62 @@
+from turtle import *
+showturtle()
+
+cislo = 5
+prirastok = 10
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+forward(cislo)
+right(90)
+cislo += prirastok
+
+
+done()
